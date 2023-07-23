74f7b5ef758252e6f9a751b2f8733bba6e6c52a7
tahakaffashbazari <139614583+tahakaffashbazari@users.noreply.github.com>
2023-07-23 12:59:57 +0330

    Add files via upload

---
+++ text.py
@@ -0,0 +1,10 @@
+import _random
+com_number=_random.randit(0,20)
+user_number=int(input("adad ra vared kon(0,20):"))
+if com_number==user_number:
+    print("eyval")
+elif com_number>user_number:
+    print("boro bala")
+elif com_number<user_number:
+    print("boro payen")
+    
